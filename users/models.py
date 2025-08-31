95f945414b3d64fbf4c2b89b9b9ae13ebcaa5a56
Aliahmed0913 <aaosman0913@gmail.com>
2025-08-31 21:35:08 +0300

    'cleanup-UnUsed_Imports'

--- users/models.py
+++ users/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser 
-from django.core.validators import RegexValidator
-from datetime import timedelta
 # Create your models here.
 
 class User(AbstractUser):
