17b99decc48a7ab25d2fcbef99a5086870bb15cf
Aliahmed0913 <aaosman0913@gmail.com>
2025-09-03 10:52:27 +0300

    'signal_to_mail_verifyCode'

--- notifications/signals.py
+++ notifications/signals.py
@@ -1,20 +1,17 @@
 from django.db import transaction
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-from users.models import User
-from users.services.verifying_code import VerificationCodeService
+from users.models import EmailCode
 from notifications.services.verification_code import send_verification_code
 
-@receiver(post_save, sender=User)
-def handle_user_registeration_verify_code(sender, instance, created, **kwargs):
+@receiver(post_save, sender=EmailCode)
+def handle_verify_code_mail(sender, instance, created, **kwargs):
     '''
-    Listener to user when any registeration event happen.
+    Listener to user verify code when any creation happen.
     
-    it's create and mail an verify code'''
+    it's mail that verify code'''
     if created :
         def on_commit():
-            service = VerificationCodeService(instance.id)
-            code = service.create_code()
-            send_verification_code(code.id)
+            send_verification_code(instance.id)  # here where celery task is started
     
         transaction.on_commit(on_commit)
