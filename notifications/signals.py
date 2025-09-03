db79b970a2796c3e25cc6d9b137d075ea5f43794
Aliahmed0913 <aaosman0913@gmail.com>
2025-09-03 14:08:41 +0300

    'handle_mailVerifyCode_creation'

--- notifications/signals.py
+++ notifications/signals.py
@@ -1,6 +1,7 @@
 from django.db import transaction
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+
 from users.models import EmailCode
 from notifications.services.verification_code import send_verification_code
 
