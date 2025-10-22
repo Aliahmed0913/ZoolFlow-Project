23f2bf80d991e16534da2a74b14b100310cd3332
Aliahmed0913 <aaosman0913@gmail.com>
2025-10-22 11:31:04 +0300

    Add names for the endpoint to ease access.

--- users/urls.py
+++ users/urls.py
@@ -6,12 +6,12 @@ from rest_framework.routers import DefaultRouter
 app_name = 'users' 
 
 routers = DefaultRouter()
-routers.register('profiles',UserProfileViewSet,basename='update-profile')
+routers.register('profiles',UserProfileViewSet,basename='user-profile')
 routers.register('verify-code',VerificationCodeViewSet, basename='verify-code')
 
-urlpatterns = [ #api/v1/users/
+urlpatterns = [ 
     path('',include(routers.urls)),
-    path('sign-up/',UserRegistrationView.as_view(),name='registeration'),
+    path('sign-up/',UserRegistrationView.as_view(),name='registration'),
     path('login/',CustomTokenObtainPairView.as_view(),name='get-token'),
     path('refresh/',TokenRefreshView.as_view(),name='refresh-token'),
     path('logout/',TokenBlacklistView.as_view(),name='block-token'),
