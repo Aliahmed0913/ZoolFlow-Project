312da511f9fc4c6af057933d952d8eaf8015994f
Aliahmed0913 <aaosman0913@gmail.com>
2025-10-22 11:44:40 +0300

    Add views for profile&address&kyc

--- customers/views.py
+++ customers/views.py
@@ -2,23 +2,28 @@ from django.shortcuts import render
 from rest_framework.generics import RetrieveUpdateDestroyAPIView
 from rest_framework.viewsets import ModelViewSet
 from customers.permissions import IsCustomerOwnership
-from customers.models import Customer, Address
+from customers.models import Customer, Address, KnowYourCustomer 
 from rest_framework.permissions import IsAuthenticated
-from customers.serializers import CustomerProfileSerializer, CustomerAddressSerializer
+from customers.serializers import CustomerProfileSerializer, CustomerAddressSerializer, KnowYourCustomerSerializer
 # Create your views here.
 
-class CustomerProfile(RetrieveUpdateDestroyAPIView):
+class CustomerProfileAPIView(RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = CustomerProfileSerializer
     def get_object(self):
-        return Customer.objects.get(user = self.request.user)
-    
+        return Customer.objects.get(user = self.request.user)    
 
-class CustomerAddress(ModelViewSet):
+class CustomerAddressViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated,IsCustomerOwnership]
     serializer_class = CustomerAddressSerializer
     def get_queryset(self):
         user = self.request.user
         addresses = Address.objects.filter(customer__user_id = user.id)
         return addresses
+
+class KnowYourCustomerAPIView(RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = KnowYourCustomerSerializer
+    def get_object(self):
+        return KnowYourCustomer.objects.get(customer = self.request.user.customer_profile)
 
