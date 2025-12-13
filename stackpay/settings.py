4521e7b5fd4c95676cc4b3141a84207ce3feccf1
Aliahmed0913 <aaosman0913@gmail.com>
2025-12-13 13:04:12 +0200

    remove sessionauth from default DRF classes

--- stackpay/settings.py
+++ stackpay/settings.py
@@ -173,8 +173,7 @@ LOGGING = {
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES':[
-        'rest_framework_simplejwt.authentication.JWTAuthentication',  
-        'rest_framework.authentication.SessionAuthentication',    
+        'rest_framework_simplejwt.authentication.JWTAuthentication', 
     ],
     
     
