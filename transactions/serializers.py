fac0b8aaac77566c4c4b7ed88096806192c5ba02
Aliahmed0913 <aaosman0913@gmail.com>
2025-12-27 20:41:55 +0200

    Generalize payment provider attribute

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -2,10 +2,11 @@ from rest_framework import serializers
 from transactions.models import Transaction
 
 class TransactionSerializer(serializers.ModelSerializer):
+    state_display = serializers.CharField(source='get_state_display',read_only=True)
     class Meta():
         model = Transaction
-        fields = ('customer','amount','status','paymob_order_id','paymob_payment_token','created_at')
-        read_only_fields = ('customer','status','paymob_order_id','paymob_payment_token')
+        fields = ('customer','transaction_id','amount','state','order_id','payment_token','created_at','state_display','merchant_order_id')
+        read_only_fields = ('customer','transaction_id','state','order_id','payment_token','merchant_order_id')
         extra_kwargs={
             'amount':{'required':True}
         }
