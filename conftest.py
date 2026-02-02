285ed445eb698d83243e7bc5d3083b968350338b
Aliahmed0913 <aaosman0913@gmail.com>
2026-02-02 18:51:53 +0200

    update root structure

--- conftest.py
+++ conftest.py
@@ -1,32 +1,37 @@
 import pytest
-from rest_framework.test import APIClient
 from django.db.models.signals import post_save
-from users.models import User
-from users.signals import handle_user_registeration_verify_code
+from django.contrib.auth import get_user_model
+from rest_framework.test import APIClient
+from users.signals import initiate_verification_code
 
-@pytest.fixture(scope='session')
+User = get_user_model()
+
+
+@pytest.fixture(scope="session")
 def api_client():
     return APIClient()
 
+
 @pytest.fixture
 def mock_mail(mocker):
-    return mocker.patch('notifications.services.verification_code.mail_code_task.delay')
+    return mocker.patch("notifications.services.verification_code.mail_code_task.delay")
 
 
+@pytest.fixture(scope="function")
+def create_activate_user(db, mocker):
+    post_save.disconnect(initiate_verification_code, sender=User)
 
-@pytest.fixture(scope='function')
-def create_activate_user(db,mocker):
-    post_save.disconnect(handle_user_registeration_verify_code,sender=User)
     def make_user(**kwargs):
         user = User.objects.create_user(
-            username = kwargs.get('username','Aliahmed'),
-            password = kwargs.get('password','Aliahmed091$'),
-            role_management = kwargs.get('role_management',User.Roles.CUSTOMER),
-            email = kwargs.get('email','example998@cloud.com'),
-            is_active=kwargs.get('is_active',False)
-          )
+            username=kwargs.get("username", "Aliahmed"),
+            password=kwargs.get("password", "Aliahmed091$"),
+            role_management=kwargs.get("role_management", User.Roles.CUSTOMER),
+            email=kwargs.get("email", "example998@cloud.com"),
+            is_active=kwargs.get("is_active", False),
+        )
         user.is_active = True
-        user.save(update_fields=['is_active'])
-        post_save.connect(handle_user_registeration_verify_code,sender=User)
+        user.save(update_fields=["is_active"])
+        post_save.connect(initiate_verification_code, sender=User)
         return user
+
     return make_user
